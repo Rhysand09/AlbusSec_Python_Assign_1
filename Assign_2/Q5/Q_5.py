22c13d027731de3e3b5e31911096f2e2d41a761a
Rhysand09 <96830299+Rhysand09@users.noreply.github.com>
2024-05-07 14:51:24 +0530

    Add files via upload

---
+++ Assign_2/Q5/Q_5.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+def generate_password(length):
+    all_characters = string.ascii_letters + string.digits + string.punctuation
+    if length < 8:
+        print("Length of the password should be at least 8")
+        return None
+    password = ''.join(random.choice(all_characters) for i in range(length))
+    return password
+
+length = int(input("Enter the length of the password: "))
+print("Generated Password: ", generate_password(length))
