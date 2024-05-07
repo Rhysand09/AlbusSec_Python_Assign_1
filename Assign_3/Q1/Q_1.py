c5bfc283df5ecc9bf06ed50b1f72aafe6b3695b2
Rhysand09 <96830299+Rhysand09@users.noreply.github.com>
2024-05-07 22:09:31 +0530

    Update Q_1.py

--- Assign_3/Q1/Q_1.py
+++ Assign_3/Q1/Q_1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-def clean_data(file_name):
-    df = pd.read_csv(file_name)
-    df.drop_duplicates(inplace=True)
-    df.fillna(method='ffill', inplace=True)
-    return df
+df = pd.read_csv('data.csv')
+
+df = df.drop_duplicates()
+
+df = df.fillna('N/A')
